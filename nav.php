74f9f2080b69f4342019c6335e21ab9fd060eab2
reynier-nr <reynier.nr@gmail.com>
2017-05-06 12:52:10 -0700

    Template init

---
+++ nav.php
@@ -0,0 +1,9 @@
+<nav id="menu">
+          <ul>
+              <li><a href="#">Inicio</a></li>
+              <li><a href="nosotros.php">Nosotros</a></li>
+              <li><a href="#">Tratamientos</a></li>
+              <li><a class="none" href="#">Servicios</a></li>
+              <li><a class="none" href="#">Contáctanos</a></li>              
+          </ul>  
+</nav>
---
+++ serv.php
@@ -0,0 +1,12 @@
+<article>
+               <h5> ! Estamos para Ayudarte ¡ </h5>
+               
+               <img class="art" src="http://lorempixel.com/200/200/abstract/" alt="">
+               
+               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quis nam ea velit, quasi eius reprehenderit magni! Sit molestias dolorum maiores repellat vero, ab corporis sed fugiat dolorem soluta veritatis!</p>
+               
+               
+               
+               <a href="#" class="btninfo service">Continuar Leyendo</a>
+               
+           </article>
