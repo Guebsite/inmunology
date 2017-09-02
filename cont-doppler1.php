5cbcb84e5e677a10436a7f8dd426b90be2d107c3
reynier-nr <reynier.nr@gmail.com>
2017-09-02 09:41:01 -0700

    finiss

--- cont-doppler1.php
+++ cont-doppler1.php
@@ -1,9 +1,9 @@
 <article>
                 <img class="homeart" src="http://lorempixel.com/200/200/abstract/" alt="">
                 
-                <h1>Amniosentesis</h1>
+                <h1>Doppler primer trimestre</h1>
                  
-                <p>---</p>
+                <p>Evaluación de la circulación Materna a la fetal mediante la revisión de artérias uterinas y así descartar problemas de pre eclampsia temprana o restricción del crecimiento intrauterino. </p>
                 
                 <a href="contacto.php" class="btninfo solve">Solicitar Información</a>
                 
